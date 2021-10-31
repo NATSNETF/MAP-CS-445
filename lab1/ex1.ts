4e196df7a57a9a0b797a7cb93ac94cbb9efe322b
Natsnet Fessehaye <natsnetf90@gmail.com>
2021-10-30 20:26:16 -0500

    lab1/ex1

---
+++ lab1/ex1.ts
@@ -0,0 +1,2 @@
+//Re-write the following code using typeScript, try to be as explict as possible and add types to everything you can. when you are done.
+//transpile the TS code to JS code and inspect the js code.
